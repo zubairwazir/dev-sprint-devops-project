97feca3e0f859fc6bd0bb15786177648bde40164
chaz_ <cbhavra@qa.com>
2021-08-10 16:31:18 +0000

    App and Tests Done

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ sudo apt-get install python3-venv -y
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r service_1/requirements.txt
-python3 -m pytest service_1/tests/test_unit1.py --cov=service_1  
-python3 -m pytest service_2/tests/test_unit2.py --cov=service_2  
-python3 -m pytest service_3/tests/test_unit3.py --cov=service_3 
-python3 -m pytest service_4/tests/test_unit4.py --cov=service_4
+python3 -m pytest service_1/tests/test_unit1.py --cov  
+python3 -m pytest service_2/tests/test_unit2.py --cov
+python3 -m pytest service_3/tests/test_unit3.py --cov 
+python3 -m pytest service_4/tests/test_unit4.py --cov
