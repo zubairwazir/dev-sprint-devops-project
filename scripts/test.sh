2bfcf8159f32dedc6de2c15e53cdc88badf06ec6
chaz_ <cbhavra@qa.com>
2021-08-12 11:08:57 +0000

    Project

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,3 +7,4 @@ python3 -m pytest service_1/tests/test_unit1.py --cov
 python3 -m pytest service_2/tests/test_unit2.py --cov
 python3 -m pytest service_3/tests/test_unit3.py --cov 
 python3 -m pytest service_4/tests/test_unit4.py --cov
+python3 -m pytest --cov
