17376c6a92a2f28f46b67decfa13db632e1593a9
chaz_ <cbhavra@qa.com>
2021-08-12 17:14:21 +0000

    Project

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -6,14 +6,14 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
-app.config['SECRET_KEY'] = 'pass'
+
     
 db = SQLAlchemy(app)
 
 from application import routes
 
-
+# Hello
 
 
